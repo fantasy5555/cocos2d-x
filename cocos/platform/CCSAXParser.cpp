b3ada3374bc1d56f7dc8080b7ecd0f5970b289fd
halx99 <halx99@live.com>
2016-08-24 14:32:25 +0800

    fix

--- cocos/platform/CCSAXParser.cpp
+++ cocos/platform/CCSAXParser.cpp
@@ -99,12 +99,12 @@ public:
         _ccsaxParserImp = parser;
     }
 
-    void xmlSAX2StartElement(const char *name, size_t len, const char **atts, size_t attslen) override
+    void xmlSAX2StartElement(const char *name, size_t /*len*/, const char **atts, size_t /*attslen*/) override
     {
-        SAXParser::startElement(_ccsaxParserImp, (const CC_XML_CHAR *)elementName.first, (const CC_XML_CHAR **)(&elementAttrs[0]));
+        SAXParser::startElement(_ccsaxParserImp, (const CC_XML_CHAR *)name, (const CC_XML_CHAR **)atts);
     }
 
-    void xmlSAX2EndElement(const char *name, size_t len) override
+    void xmlSAX2EndElement(const char *name, size_t /*len*/) override
     {
         SAXParser::endElement(_ccsaxParserImp, (const CC_XML_CHAR *)name);
     }
